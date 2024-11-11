a4c759c12ac43e6e066ca7565be7fa692533942f
BRUNO <bdnok520@outlook.fr>
2024-11-11 09:52:40 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*-coding:Latin-1 -*
+import os # On importe le module os
+print("Bonjour le monde ! ")
+print (" ")
+print (" ")
+
+from utils.multipli import *
+
+# test de la fonction table
+table(3, 20)
+racine(9)
+
+
+os.system("pause")
